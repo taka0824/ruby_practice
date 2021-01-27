6725db4c5676826f565c31915323b234b1b1d972
Takanari Nishio <taka0824nari@gmail.com>
2021-01-27 13:25:33 +0000

    pyramid method

--- pyramid2.rb
+++ pyramid2.rb
@@ -1,12 +1,12 @@
-# def pyramid(n)
-#   1.upto(n) { |x|
-#     puts(" "*(n-x) + "*"*(2*x-1))
-#   }
-# end
+def pyramid(n)
+  1.upto(n) { |x|
+    puts(" "*(n-x) + "*"*(2*x-1))
+  }
+end
 
-# pyramid(4)
+pyramid(4)
 
 
   for i in 1..5 do
-    puts(" " * (5-i) + "*" * (2 * i-1))
+    puts(" " * (5-i) + "*" * (2 * i -1))
   end
